13e76df8cb19e7879e2796d9c50e9c7244ef242e
Z1springer <zdspringer99@gmail.com>
2025-01-10 18:21:56 -0800

    initially added color npm package but then removed it and replaced it with colors npm package

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import lodash from "lodash";
 import chalk from "chalk";
+import colors from "colors";
+
 const array = [
   1, 1, 3, 4, 5, 6, 6, 7, 8, 9, 10, 12, 13, 14, 15, 15, 16, 17, 18, 19, 20,
 ];
@@ -9,3 +11,5 @@ const uniqueNums = lodash.uniq(array);
 console.log(uniqueNums);
 
 console.log(chalk.blueBright("A Colourful Hello from Chalk!"));
+
+console.log(colors.america("USA!!!"));
