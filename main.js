781652076e6d0679631d27febeef83b429d95396
Z1springer <zdspringer99@gmail.com>
2025-01-10 18:05:33 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import lodash from "lodash";
+import chalk from "chalk";
+const array = [
+  1, 1, 3, 4, 5, 6, 6, 7, 8, 9, 10, 12, 13, 14, 15, 15, 16, 17, 18, 19, 20,
+];
+
+const uniqueNums = lodash.uniq(array);
+
+console.log(uniqueNums);
+
+console.log(chalk.blueBright("A Colourful Hello from Chalk!"));
